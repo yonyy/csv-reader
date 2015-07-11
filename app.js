628f80bf28cf2d2a9144aa3a9e33f852339f7027
Yonatan Perez <yonatan.perez.0213@gmail.com>
2015-07-10 23:41:13 -0700

    restructed folders. added model schemas, added option to customize roster, included angularjs in student.jade

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var bodyParser = require("body-parser");
+var logger = require('morgan');
+
+var db = require('./model/db');
+var classrooms = require('./model/classroom');
+var students = require('./model/student');
+var seats = require('./model/seat');
+
+var routes = require('./routes/index');
+var classroom = require('./routes/classroom');
+var student = require('./routes/student');
+
 var http = require('http').createServer(app);
-var classroom_module = require('./classroom_module');
-var site = require('./site.js');
 var methodOverride = require('method-override');
 
 /* Server config */
 app.set("ipaddr", "127.0.0.1");
 app.set("port",8080);
 app.set('view engine', 'jade');
-app.set('views',__dirname + '/views');
-app.use(express.static(__dirname+'/public'));
-app.use(express.static(__dirname+'/views/js'));
+app.set('views', path.join(__dirname + '/views'));
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + '/public/javascript')));
+app.use(express.static(path.join(__dirname + '/public/stylesheets')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 /* Send home page file */
-app.get('/', site.index);
-app.get('/classroom', classroom_module.view);
-app.get('/classroom/seating_chart', classroom_module.view_chart)
+app.use('/', routes);
+app.use('/classroom', classroom);
+app.use('/student', student);
+
 
 http.listen(app.get("port"), app.get("ipaddr"), function() {
   console.log("Server up and running. Go to http://" + app.get("ipaddr") + ":" + app.get("port"));
 });
+
+module.exports = app;
--- views/js/student.js
+++
@@ -1,8 +0,0 @@
-function Student(lastname, firstname, email, isLH, isOSD, seat)	{
-	this.lastname = lastname
-	this.firstname = firstname
-	this.email = email
-	this.isLeftHanded = isLH
-	this.isOSD = isOSD
-	this.seat = seat
-}
