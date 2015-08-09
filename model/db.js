cc02501f22b9ee6ec34e0162fe4d6600515d0623
Yonatan Perez <yonatan.perez.0213@gmail.com>
2015-08-08 17:27:31 -0700

    connecting to mongoLab using heroku

--- model/db.js
+++ model/db.js
@@ -1,2 +1,20 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/csvDB');
+/*var http = require('http');*/
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.
+var uristring =
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/csvDB';
+
+// The http server will listen to an appropriate port, or default to
+// port 5000.
+/*var theport = process.env.PORT || 5000;*/
+
+mongoose.connect(uristring, function (err, res) {
+	if (err)
+		console.log('ERROR connecting to: ' + uristring + '. ' + err)
+	else
+		console.log('Succeeded connected to: ' + uristring)
+});
